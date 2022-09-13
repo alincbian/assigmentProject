379ddcd8e49730285c4694769c0e408d8c1e45cc
Ali Raza <95338856+raza-Viltco@users.noreply.github.com>
2022-09-13 14:25:03 +0500

    new change

--- index.js
+++ index.js
@@ -1,60 +1,78 @@
-console.log(a)
-let a;
+// console.log(a)
+// let a;
 
 
-const test = [
-    { x: "foo", y: 8 },
-    { x: "bar", y: 3 },
-    { x: "baz", y: 7 },
-  ];
+// const test = [
+//     { x: "foo", y: 8 },
+//     { x: "bar", y: 3 },
+//     { x: "baz", y: 7 },
+//   ];
   
-  const foo = test.reduce(
-    (a, b) => ({
-      ...a,
-      [b.x]: b.y,
-    }),
-    {}
-  );
+//   const foo = test.reduce(
+//     (a, b) => ({
+//       ...a,
+//       [b.x]: b.y,
+//     }),
+//     {}
+//   );
 
-  console.log(foo)
+//   console.log(foo)
 
 
-  var x = { foo : 1};
-var output = (function() {
-  delete x.foo;
-  return x.foo;
-})();
+//   var x = { foo : 1};
+// var output = (function() {
+//   delete x.foo;
+//   return x.foo;
+// })();
 
-console.log(output);
+// console.log(output);
 
-var trees = ["xyz", "xxxx", "test", "ryan", "apple"];
-delete trees[3];
-// console.log(trees)
-console.log(trees.length);
+// var trees = ["xyz", "xxxx", "test", "ryan", "apple"];
+// delete trees[3];
+// console.log(trees.length);
 
-var salary = "1000$";
+// var salary = "1000$";
 
-(function () {
-  console.log("Original salary was " + salary);
+// (function () {
+//   console.log("Original salary was " + salary);
 
-  var salary = "5000$";
+//   var salary = "5000$";
 
-  console.log("My New Salary " + salary);
-})();
+//   console.log("My New Salary " + salary);
+// })();
 
 // var a = 10
 // var a =7
 // const c= 4;
 
 
-function b() {
-var a = 5;
-// return console.log(a);
+// function b() {
+// var a = 5;
+// // return console.log(a);
+// }
+
+// b();
+
+
+// console.log(a)
+// console.log(c)
+
+let arr = "madam";
+
+const revVal = () =>
+{
+var arrd = arr.split().reverse();
+  var arrd1 = arr;
+
+ if(arrd === arrd1)
+ {
+  console.log("plandrome")
+ }
+ else
+ {
+  console.log("not a plandrome")
+ }
 }
 
-b();
-
-
-console.log(a)
-// console.log(c)
+revVal();
 
